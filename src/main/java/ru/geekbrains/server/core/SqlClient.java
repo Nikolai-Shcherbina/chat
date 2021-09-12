f101cb1ae298bb1f68fe8a1a309396bb56a9b14a
Nikolai Shcherbina <nikvikshch@gmail.com>
2021-09-12 17:15:25 +0300

    ДЗ № 2 - авторизация через базу данных SQLite/

--- src/main/java/ru/geekbrains/server/core/SqlClient.java
+++ src/main/java/ru/geekbrains/server/core/SqlClient.java
@@ -18,15 +18,25 @@ public class SqlClient {
     }
 
     synchronized static void disconnect() {
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        if(statement != null){
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if(connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     synchronized static String getNickname(String login, String password) {
-        String query = String.format("select nickname from users where login='%s' and password='%s'", login, password);
+//        String query = String.format("select nickname from users where login='%s' and password='%s'", login, password);
+        String query = String.format("select nickname from new_users where login='%s' and password='%s'", login, password);
         try (ResultSet set = statement.executeQuery(query)) {
             if (set.next())
                 return set.getString(1);
@@ -35,6 +45,25 @@ public class SqlClient {
         }
         return null;
     }
+    private void createTable() throws SQLException {
+        statement.executeUpdate("create table if not exists new_users(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "login STRING," +
+                "password STRING," +
+                "nickname STRING)");
+    }
+    private void insert(String login, String pass, String nickname) throws SQLException {
+        statement.executeUpdate(String.format("insert into new_users(login, password, nickname) values('%s', '%s', '%s')", login, pass, nickname));
+    }
+    public static void main(String[] args) {
+        SqlClient sqlClient = new SqlClient();
+        connect();
+        try {
+            sqlClient.createTable();
+            sqlClient.insert("Sacha", "123111", "Straik" );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-
+    }
 }
